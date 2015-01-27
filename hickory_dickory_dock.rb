9997f2ec8e8d71c691bbb781f7f3fee4feb4147f
jendiamond <thejendiamond@gmail.com>
2015-01-27 14:17:22 -0800

    Add Jen's hickory_dickory_dock program

---
+++ hickory_dickory_dock.rb
@@ -0,0 +1,142 @@
+class Clock
+
+def initialize
+ one
+two
+end
+
+#print "Pick a number from 1 to 12. >> "
+#time = gets.chomp.to_i
+#puts one
+
+def one
+if time == 1
+puts "Cuckoo, cuckcoo. You chose #{time}."
+puts """
+Hickory dickory dock,
+The mouse ran up the clock.
+The clock struck #{time.to_s},
+The mouse ran down,
+Hickory dickory dock.
+"""
+end
+end
+
+def two
+if time == 2
+puts "Cuckoo, cuckcoo. You chose #{time}."
+puts """
+Hickory Dickory Dock,
+The bird looked at the clock, 
+The clock struck two,
+Away she flew,
+Hickory Dickory Dock.
+"""
+end
+end
+
+if time ==3
+puts "Cuckoo, cuckcoo. You chose #{time}."
+puts """
+Hickory Dickory Dock,
+The dog barked at the clock,
+The clock struck three,
+Fiddle-de-dee,
+Hickory Dickory Dock.
+"""
+
+elsif time == 4
+puts "Cuckoo, cuckcoo. You chose #{time}."
+puts """
+Hickory Dickory Dock,
+The bear slept by the clock,
+The clock struck four,
+He ran out the door,
+Hickory Dickory Dock.
+"""
+
+elsif time == 5
+puts "Cuckoo, cuckcoo. You chose #{time}."
+puts """
+Hickory Dickory Dock,
+The bee buzzed 'round the clock,
+The clock struck five,
+She went to her hive,
+Hickory Dickory Dock.
+"""
+
+elsif time == 6
+puts "Cuckoo, cuckcoo. You chose #{time}."
+puts """
+Hickory Dickory Dock,
+The hen pecked at the clock,
+The clock struck six,
+Oh, fiddle-sticks,
+Hickory Dickory Dock.
+"""
+elsif time == 7
+puts "Cuckoo, cuckcoo. You chose #{time}."
+puts """
+Hickory Dickory Dock,
+The cat ran round the clock,
+The clock struck seven,
+She wanted to get 'em,
+Hickory Dickory Dock.
+"""
+
+elsif time == 8
+puts "Cuckoo, cuckcoo. You chose #{time}."
+puts """
+Hickory Dickory Dock,
+The horse jumped over the clock,
+The clock struck eight,
+He ate some cake,
+Hickory Dickory Dock.
+"""
+
+elsif time == 9
+puts "Cuckoo, cuckcoo. You chose #{time}."
+puts """
+Hickory Dickory Dock,
+The cow danced on the clock,
+The clock struck nine,
+She felt so fine,
+Hickory Dickory Dock.
+"""
+elsif time == 10
+puts "Cuckoo, cuckcoo. You chose #{time}."
+puts """
+Hickory Dickory Dock,
+The pig oinked at the clock,
+The clock struck ten,
+She did it again,
+Hickory Dickory Dock.
+"""
+
+elsif time == 11
+puts "Cuckoo, cuckcoo. You chose #{time}."
+puts """
+Hickory Dickory Dock,
+The duck quacked at the clock
+The clock struck eleven,
+The duck said 'Oh Heavens!'
+Hickory Dickory Dock.
+"""
+
+elsif time == 12
+puts "Cuckoo, cuckcoo. You chose #{time}."
+puts """
+Hickory Dickory Dock,
+The mouse ran up the clock
+The clock struck noon,
+He\'s here too soon!
+Hickory Dickory Dock.
+"""
+end
+end
+
+Clock.new.one
+Clock.new.two
+omp.to_i
+print "Pick a number from 1 to 12. >> "
+Clock.new
