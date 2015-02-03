02114663c837cffc9c172e207715d08030e67da7
jendiamond <thejendiamond@gmail.com>
2015-02-02 18:36:32 -0800

    Change from class

--- hickory_dickory_dock.rb
+++ hickory_dickory_dock.rb
@@ -1,66 +1,65 @@
 print "Pick a number from 1 to 12. >> "
 time = gets.chomp.to_i
-puts one
 
 if time == 1
-puts "Cuckoo, cuckcoo. You chose #{time}."
-puts """
-Hickory dickory dock,
-The mouse ran up the clock.
-The clock struck #{time.to_s},
-The mouse ran down,
-Hickory dickory dock.
-"""
+  puts "Cuckoo, cuckcoo. You chose #{time}."
+  puts """
+    Hickory dickory dock,
+    The mouse ran up the clock.
+    The clock struck #{time.to_s},
+    The mouse ran down,
+    Hickory dickory dock.
+  """
 
-if time == 2
-puts "Cuckoo, cuckcoo. You chose #{time}."
-puts """
-Hickory Dickory Dock,
-The bird looked at the clock, 
-The clock struck two,
-Away she flew,
-Hickory Dickory Dock.
-"""
+elsf time == 2
+  puts "Cuckoo, cuckcoo. You chose #{time}."
+  puts """
+    Hickory Dickory Dock,
+    The bird looked at the clock, 
+    The clock struck two,
+    Away she flew,
+    Hickory Dickory Dock.
+  """
 
-if time ==3
-puts "Cuckoo, cuckcoo. You chose #{time}."
-puts """
-Hickory Dickory Dock,
-The dog barked at the clock,
-The clock struck three,
-Fiddle-de-dee,
-Hickory Dickory Dock.
-"""
+elsif time ==3
+  puts "Cuckoo, cuckcoo. You chose #{time}."
+  puts """
+    Hickory Dickory Dock,
+    The dog barked at the clock,
+    The clock struck three,
+    Fiddle-de-dee,
+    Hickory Dickory Dock.
+  """
 
 elsif time == 4
-puts "Cuckoo, cuckcoo. You chose #{time}."
-puts """
-Hickory Dickory Dock,
-The bear slept by the clock,
-The clock struck four,
-He ran out the door,
-Hickory Dickory Dock.
-"""
+  puts "Cuckoo, cuckcoo. You chose #{time}."
+  puts """
+    Hickory Dickory Dock,
+    The bear slept by the clock,
+    The clock struck four,
+    He ran out the door,
+    Hickory Dickory Dock.
+  """
 
 elsif time == 5
-puts "Cuckoo, cuckcoo. You chose #{time}."
-puts """
-Hickory Dickory Dock,
-The bee buzzed 'round the clock,
-The clock struck five,
-She went to her hive,
-Hickory Dickory Dock.
-"""
+  puts "Cuckoo, cuckcoo. You chose #{time}."
+  puts """
+    Hickory Dickory Dock,
+    The bee buzzed 'round the clock,
+    The clock struck five,
+    She went to her hive,
+    Hickory Dickory Dock.
+  """
 
 elsif time == 6
-puts "Cuckoo, cuckcoo. You chose #{time}."
-puts """
-Hickory Dickory Dock,
-The hen pecked at the clock,
-The clock struck six,
-Oh, fiddle-sticks,
-Hickory Dickory Dock.
-"""
+  puts "Cuckoo, cuckcoo. You chose #{time}."
+  puts """
+    Hickory Dickory Dock,
+    The hen pecked at the clock,
+    The clock struck six,
+    Oh, fiddle-sticks,
+    Hickory Dickory Dock.
+  """
 elsif time == 7
 puts "Cuckoo, cuckcoo. You chose #{time}."
 puts """
@@ -119,3 +118,4 @@ The clock struck noon,
 He\'s here too soon!
 Hickory Dickory Dock.
 """
+end
