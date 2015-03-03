7185c117699b5500a7294a7bea5549012fedcad7
KalisaFalzone <kalisa.falzone@gmail.com>
2015-03-02 19:16:58 -0800

    Change ticktock.rb so that multiples of 15 print ticktock on one line.

--- ticktock.rb
+++ ticktock.rb
@@ -1,9 +1,12 @@
 (1..100).each do |i|
-  if i%3 == 0
+  if i%3 == 0 and i%5 != 0
     puts 'tick'
   end
-  if i%5 == 0
+  if i%5 == 0 and i%3 != 0
     puts 'tock'
+  end
+  if i%15 == 0
+    puts 'ticktock'
   end
   if i%3 !=0 && i%5 !=0
   puts i
